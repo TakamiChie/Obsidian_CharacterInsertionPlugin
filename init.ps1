d5d67d182ce178d407e4a4e7b2164a18bbda4e03
高見知英 <chie@onpu-tamago.net>
2023-04-02 17:30:58 +0900

    指示に従いmanifest.jsonを移動

--- init.ps1
+++ init.ps1
@@ -1,3 +1,4 @@
 $dir = (Convert-Path "$PSScriptRoot\")
 $name = Split-Path "${dir}" -Leaf
 New-Item -Value "${dir}src" -Path "${dir}DevTest\.obsidian\plugins\${name}" -ItemType Junction
+New-Item -Value "${dir}manifest.json" -Path "${dir}src/manifest.json" -ItemType HardLink
